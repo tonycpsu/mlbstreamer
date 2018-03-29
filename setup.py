957dd61feac800f5b174e35ec4e906baf4cf6939
Tony Cebzanov <tonycpsu@gmail.com>
2018-03-29 09:24:50 -1000

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from glob import glob
 
 name = "mlbstreamer"
 setup(name=name,
-      version="0.0.1",
+      version="0.0.2",
       description="MLB.tv Stream Browser",
       author="Tony Cebzanov",
       author_email="tonycpsu@gmail.com",
