d5e1d55a017203c4528fff85b6d3666df6984051
Matthias Küch <mail@matthias-kuech.de>
2018-04-02 13:27:19 +0200

    Rephrased log messages

--- mlbstreamer/session.py
+++ mlbstreamer/session.py
@@ -135,7 +135,7 @@ class MLBSession(object):
 
     def login(self):
 
-        logger.debug("logging in")
+        logger.debug("attempting to log in")
 
         initial_url = ("https://secure.mlb.com/enterworkflow.do"
                        "?flowId=registration.wizard&c_id=mlb")
@@ -154,7 +154,8 @@ class MLBSession(object):
         if self.logged_in:
             logger.debug("already logged in")
             return
-        logger.debug("logging in")
+
+        logger.debug("sucessfully logged in")
 
         login_url = "https://securea.mlb.com/authenticate.do"
 
